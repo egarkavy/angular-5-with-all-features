b7872a7381141d070f38807fdf6f0bffa92b747d
EgorGarkavy <egarkavy@det-team.com>
2018-05-29 11:16:24 +0300

    use home component in appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
  
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 @NgModule({
   imports: [
-    BrowserModule, RouterModule.forRoot()
+    BrowserModule, RouterModule.forRoot(routes)
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   bootstrap: [ AppComponent ]
 })
